7c4df122fc9926968e1245dd855725185ff21fb0
Tom Sarver <tom@tomsarver.dev>
2022-02-09 16:53:22 -0600

    adding animation and toggling ability to hamburger menu

--- src/components/NavigationMobile.vue
+++ src/components/NavigationMobile.vue
@@ -1,6 +1,6 @@
 <template>
   <div ref="component">
-    <div class="hamburger">
+    <div ref="hamburger" class="hamburger" @click="toggleHamburger()">
       <div class="hamburger-patty"></div>
       <div class="hamburger-patty"></div>
       <div class="hamburger-patty"></div>
@@ -16,6 +16,17 @@ import Navigation from "@/components/Navigation";
 
 export default {
   name: 'NavigationMobile',
+  methods: {
+    async toggleHamburger(){
+      if (this.$refs.hamburger.classList.contains('hamburger-active')) {
+        this.$refs.hamburger.classList.remove('hamburger-active');
+      }
+      else
+      {
+        this.$refs.hamburger.classList.add('hamburger-active');
+      }
+    }
+  },
   components: {
     Navigation
   },
@@ -25,10 +36,11 @@ export default {
 <style scoped lang="scss">
 .hamburger{
   display: flex;
-  flex-flow: column-reverse;
+  flex-flow: column;
 
-  height: 3rem;
   width: 2.5rem;
+
+  cursor: pointer;
 
   .hamburger-patty{
     height: .35rem;
@@ -37,15 +49,27 @@ export default {
     margin: .2rem 0;
     border-radius: 1rem;
 
+    background-color: white;
+  }
+}
+
+.hamburger-active{
+  .hamburger-patty{
+    transition:  all .35s ease;
+
     &:nth-child(1){
-      background-color: white;
+      transform: translateY(.75rem) translateX(0) rotate(45deg);
     }
     &:nth-child(2){
-      background-color: white;
+      opacity: 0;
     }
     &:nth-child(3){
-      background-color: white;
+      transform: translateY(-.75rem) translateX(0) rotate(-45deg);
     }
   }
+}
+
+.mobile-nav-wrapper{
+  position: fixed;
 }
 </style>
