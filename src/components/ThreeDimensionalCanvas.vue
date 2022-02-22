f923da04c2b5d6f91879c3988dc77147173bd9d2
Tom Sarver <tom@tomsarver.dev>
2022-02-21 18:39:59 -0600

    getting 3D stuff working

---
+++ src/components/ThreeDimensionalCanvas.vue
@@ -0,0 +1,28 @@
+<template>
+  <Renderer ref="renderer" resize="window">
+    <Camera :position="{ z: 10 }" />
+    <Scene>
+      <PointLight :position="{ y: 50, z: 50 }" />
+      <Box ref="box" :rotation="{ y: Math.PI / 4, z: Math.PI / 4 }">
+        <LambertMaterial />
+      </Box>
+    </Scene>
+  </Renderer>
+</template>
+
+<script>
+
+export default {
+  name: 'ThreeDimensionalCanvas',
+  mounted() {
+    const renderer = this.$refs.renderer;
+    const box = this.$refs.box.mesh;
+    renderer.onBeforeRender(() => {
+      box.rotation.x += 0.01;
+    });
+  },
+};
+</script>
+
+
+<style scoped lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue';
 import App from './App'
+
+/*Plugins*/
 import Router from '@/router/router';
+import {TroisJSVuePlugin} from 'troisjs';
 
 const app = createApp(App);
 
 app.use(Router);
+app.use(TroisJSVuePlugin);
 
 app.mount('#app');
